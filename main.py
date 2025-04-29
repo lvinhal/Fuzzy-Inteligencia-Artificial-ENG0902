02a09db652b432a62d20d16821382930bd5f3f43
Lucas Vinhal <74068547+lvinhal@users.noreply.github.com>
2025-04-29 13:41:57 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from modules.interface import AvaliacaoFuzzyApp
-
-# Iniciar a aplicação
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = AvaliacaoFuzzyApp(root)
-    root.mainloop()
